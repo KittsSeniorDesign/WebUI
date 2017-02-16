81eccd3ebc23049662a8aee9ee6c23c0019785e5
Marton Demeter <marton1029@gmail.com>
2017-02-16 09:34:11 -0800

    add and remove robot works, as well as highlighting

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 var number_of_robots = 0;
 var number_of_fields = 7;
+var field_line_height = 18;
 var single_robot_string = false;
 var ws;
 
 function addButtonCreate() {
+    var btn_helper = document.createElement('div');
+    btn_helper.style.height = '18px';
+    
+    var title = document.createElement('div');
+    title.appendChild(document.createTextNode('Robot Container'));
+    title.style.width = '100%';
+    title.style.textAlign = 'center';
+    title.style.height = '18px';
+    
+    var robot_item_container = document.createElement('div');
+    robot_item_container.id = 'robot-item-container';
+    var h = document.getElementById('robot-container').offsetHeight - 18;
+    robot_item_container.style.width = '100%';
+    robot_item_container.style.height = h + 'px';
+    robot_item_container.style.backgroundColor = 'white';
+    
+    var btn_container = document.createElement('div');
+    btn_container.style.zIndex = 1;
+    btn_container.style.float = 'right';
+    btn_container.style.height = '18px';
+    btn_container.style.width = '18px';
+    btn_container.style.position = 'relative';
+    btn_container.style.top = '-19px';
+    
     var btn = document.createElement('button');
     btn.type = 'button';
-    btn.className = 'addRobotButton';
-    btn.style.display = 'inline-block';
-    btn.style.float = 'left';
-    btn.id = 'addRobotButton';
+    btn.style.height = '18px';
+    btn.style.width = '18px';
+    btn.style.textAlign = 'center';
+    btn.style.verticalAlign = 'middle';
+    btn.style.padding = '0';
+    btn.style.border = 'none';
+    btn.style.outline = 'none';
+    btn.style.backgroundColor = 'white';
+    btn.style.color = 'black';
     btn.onclick = addRobot;
-    var btn_txt = document.createTextNode('+');
-    btn.appendChild(btn_txt);
-    var div = document.getElementById('robot-container');
-    div.appendChild(btn);
+    btn.innerHTML = '&#43;';
+    
+    btn_container.appendChild(btn);
+    btn_helper.appendChild(title);
+    btn_helper.appendChild(btn_container);
+    document.getElementById('robot-container').appendChild(btn_helper);
+    document.getElementById('robot-container').appendChild(robot_item_container);
 }
 function modalCreate() {
     var modal = document.createElement('div');
@@ -28,9 +61,8 @@ function modalCreate() {
     modal_content_header.className = 'modal-content-header';
     var modal_content_close = document.createElement('span');
     modal_content_close.className = 'close';
+    modal_content_close.innerHTML = '&times;'
     modal_content_close.onclick = function(){document.getElementById('modal-div').style.display = 'none'};
-    var modal_content_close_txt = document.createTextNode('x');
-    modal_content_close.appendChild(modal_content_close_txt);
     modal_content_header.appendChild(modal_content_close);
     modal_content.appendChild(modal_content_header);
     modal.appendChild(modal_content);
@@ -46,8 +78,166 @@ function modalOptions() {
     title.appendChild(title_txt);
     modal_content_header.appendChild(title);
 }
-function addRobot() {
+function addRobotOptions() {
     document.getElementById('modal-div').style.display = 'block';
+}
+function addRobot(robot_number = number_of_robots) {
+    number_of_robots += 1;
+    var container = document.getElementById('robot-item-container');
+    var rc = document.createElement('div');
+    rc.style.width = '90%';
+    rc.style.height = (((number_of_fields + 1) * field_line_height) + 2 )+ 'px';
+    rc.style.margin = '0 auto';
+    rc.style.border = '1px solid black';
+    rc.style.outline = 'none';
+    rc.style.marginTop = '10px';
+    rc.id = 'rc_' + number_of_robots;
+    rc.className = 'robot';
+    var cap = document.createElement('div');
+    cap.style.height = field_line_height + 'px';
+    cap.style.textAlign = 'center';
+    cap.style.borderBottom = '1px solid black';
+    cap.style.width = '100%';
+    cap.id = 'cap_' + number_of_robots
+    var btn = document.createElement('button');
+    btn.type = 'button';
+    btn.className = 'btn_r_' + number_of_robots;
+    btn.style.position = 'relative';
+    btn.id = 'remove_robot_button_' + number_of_robots;
+    btn.style.top = '-' + field_line_height + 'px'
+    btn.style.height = field_line_height + 'px';
+    btn.style.width = field_line_height + 'px';
+    btn.style.textAlign = 'center';
+    btn.style.verticalAlign = 'middle';
+    btn.style.padding = '0';
+    btn.style.border = 'none';
+    btn.style.borderBottom = '1px solid black';
+    btn.style.outline = 'none';
+    btn.style.backgroundColor = 'white';
+    btn.style.float = 'right';
+    btn.style.color = 'black';
+    btn.style.zIndex = '1';
+    btn.onclick = removeRobot;
+    btn.innerHTML = '-';
+    cap.innerHTML = 'Robot ' + number_of_robots;
+    var field_flex_container = document.createElement('div');
+    field_flex_container.style.display = 'flex';
+    field_flex_container.style.float = 'left';
+    field_flex_container.style.flexDirection = 'column';
+    var value_flex_container = document.createElement('div');
+    value_flex_container.style.display = 'flex';
+    value_flex_container.style.float = 'right';
+    value_flex_container.style.flexDirection = 'column';
+    value_flex_container.style.position = 'relative';
+    value_flex_container.style.right = '-' + field_line_height + 'px';
+    rc.appendChild(cap);
+    rc.appendChild(btn);
+    rc.appendChild(field_flex_container);
+    rc.appendChild(value_flex_container);
+    for(var i = 0; i < number_of_fields; i++) {
+        var current_field = document.createElement('div');
+        current_field.style.height = field_line_height + 'px';
+        current_field.style.textAlign = 'left';
+        current_field.style.margin = '0 auto';
+        current_field.style.float = 'left';
+        current_field.style.paddingLeft = '2px';
+        current_field.style.display = 'block';
+        current_field.id = 'field_' + i + '_r_' + number_of_robots;
+        current_field.appendChild(document.createTextNode('Field_Name'));
+        var current_value = document.createElement('div');
+        current_value.style.height = field_line_height + 'px';
+        current_value.style.textAlign = 'right';
+        current_value.style.margin = '0 auto';
+        current_value.style.display = 'block';
+        current_value.style.float = 'right';
+        current_value.style.paddingRight = '2px';
+        current_value.id = 'value_' + i + '_r_' + number_of_robots;
+        current_value.appendChild(document.createTextNode('Value'));
+        field_flex_container.appendChild(current_field);
+        value_flex_container.appendChild(current_value);
+    }
+    container.appendChild(rc);
+}
+function removeRobot() {
+    number_of_robots--;
+    robot_number_removed = parseInt(this.id.charAt(this.id.length-1));
+    this.parentElement.parentElement.removeChild(document.getElementById('rc_' + robot_number_removed));
+    for(var i = robot_number_removed + 1; i < (number_of_robots + 2); i++) {
+        document.getElementById('rc_' + i).id = 'rc_' + (i - 1);
+        document.getElementById('remove_robot_button_' + i).id = 'remove_robot_button_' + (i - 1);
+        document.getElementById('cap_' + i).id = 'cap_' + (i - 1);
+        document.getElementById('cap_' + (i - 1)).innerHTML = 'Robot ' + (i - 1);
+        for(var j = 0; j < number_of_fields; j++) {
+            document.getElementById('field_' + j + '_r_' + i).id = 'field_' + j + '_r_' + (i - 1);
+            document.getElementById('value_' + j + '_r_' + i).id = 'value_' + j + '_r_' + (i - 1);
+        }
+    }
+}
+function displaySettings() {
+    
+}
+function pageTitleCreate() {
+    var title = document.createElement('div');
+    title.id = 'title-div';
+    title.style.margin = '0 auto';
+    title.style.height = '7vh';
+    title.style.textAlign = 'center';
+    title.style.fontSize = '30px';
+    var title_txt = document.createTextNode('Multi-bot Easy Control Hierarchy');
+    title.appendChild(title_txt);
+    document.getElementsByTagName('body')[0].appendChild(title);
+}
+function pageTableCreate() {
+    var tbl_div = document.createElement('div');
+    tbl_div.id = 'page_table-div';
+    var tbl = document.createElement('table');
+    tbl.id = 'page-table';
+    tbl.className = 'page-table';
+    var row_1 = document.createElement('tr');
+    row_1.id = 'page_table_row_1';
+    var col_11 = document.createElement('td');
+    col_11.id = 'page_table_col_11';
+    col_11.rowSpan = 2;
+    var col_12 = document.createElement('td');
+    col_12.id = 'page_table_col_12';
+    var row_2 = document.createElement('tr');
+    row_2.id = 'page_table_row_2';
+    var col_21 = document.createElement('td');
+    col_21.id = 'page_table_col_21';
+    var col_22 = document.createElement('td');
+    col_22.id = 'page_table_col_22';
+    
+    var robot_container = document.createElement('div');
+    robot_container.id = 'robot-container';
+    
+    var settings_container = document.createElement('div');
+    settings_container.id = 'settings-container';
+    var settings_container_title = document.createElement('div');
+    settings_container_title.appendChild(document.createTextNode('Settings Container'));
+    settings_container_title.style.width = '100%';
+    settings_container_title.style.textAlign = 'center';
+    settings_container_title.style.height = '18px';
+    settings_container.appendChild(settings_container_title);
+    
+    var graphics_container = document.createElement('div');
+    graphics_container.id = 'graphics-container';
+    var graphics_container_title = document.createElement('div');
+    graphics_container_title.appendChild(document.createTextNode('Graphics Container'));
+    graphics_container_title.style.width = '100%';
+    graphics_container_title.style.textAlign = 'center';
+    graphics_container_title.style.height = '18px';
+    graphics_container.appendChild(graphics_container_title);
+    
+    col_11.appendChild(robot_container);
+    col_12.appendChild(graphics_container);
+    col_21.appendChild(settings_container);
+    row_1.appendChild(col_11);
+    row_1.appendChild(col_12);
+    row_2.appendChild(col_21);
+    tbl.appendChild(row_1);
+    tbl.appendChild(row_2);
+    tbl_div.appendChild(tbl);
+    document.getElementsByTagName('body')[0].appendChild(tbl_div);
 }
 
 function tableCreate() {
@@ -114,7 +304,6 @@ function tableCreate() {
     var btn = document.getElementById('addRobotButton');
     div.insertBefore(tbl,btn);
 }
-
 function placeContent(content_array) {
     var current_var;
     var i = 0;
@@ -150,6 +339,8 @@ function placeContent(content_array) {
     }
 }
 
+pageTitleCreate();
+pageTableCreate();
 addButtonCreate();
 modalCreate();
 
