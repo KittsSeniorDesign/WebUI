17b0a3f45a37d68ca110081a756bd3766dfd628e
Marton Demeter <marton1029@gmail.com>
2017-03-09 20:15:11 -0800

    added extra error checking, synchronous logging

--- server/logger.js
+++ server/logger.js
@@ -1,49 +1,87 @@
 const fs = require('fs');
 
-var log = function(message, level, callback) {
-    if(process.platform === 'darwin') {
-        path = __dirname + '/server.log';
-        time = Date().split(' ').slice(0,-2).join(' ');
-        line_ending = '\n';
-    }
-    else if(process.platform === 'win32') {
-        path = __dirname + '\\server.log';
-        time = Date().split(' ').slice(0,-4).join(' ');
-        line_ending = '\r\n';
-    }
-    var l = 'Error';
-    switch(level) {
-        case 'error':
-            l = 'ERROR';
-            break;
-        case 'warning':
-            l = 'WARNING';
-            break;
-        case 'debug':
-            l = 'DEBUG';
-            break;
-        case 'info':
-            l = 'INFO';
-            break;
+exports.log = (message, level, callback) => {
+  if(process.platform === 'darwin') {
+    path = __dirname + '/server.log';
+    time = Date().split(' ').slice(0,-2).join(' ');
+    line_ending = '\n';
+  }
+  else if(process.platform === 'win32') {
+    path = __dirname + '\\server.log';
+    time = Date().split(' ').slice(0,-4).join(' ');
+    line_ending = '\r\n';
+  }
+  var l = 'ERROR';
+  switch(level) {
+    case 'error':
+      l = 'ERROR';
+      break;
+    case 'warning':
+      l = 'WARNING';
+      break;
+    case 'debug':
+      l = 'DEBUG';
+      break;
+    case 'info':
+      l = 'INFO';
+      break;
+  }
+  fs.open(path, 'a', function(error, fd) {
+    if (error) {
+      console.error(`Couldn't open log file: ${path}`);
+      return;
     }
     var log_message = `${time} - ${l}: ${message}.${line_ending}`;
     var buffer = new Buffer(log_message);
-    fs.open(path, 'a', function(error, fd) {
-        if (error) {
-            console.error(`Couldn't open log file: ${path}`);
-            return;
-        }
-        fs.write(fd, buffer, 0, buffer.length, null, function(error) {
-            if (error) {
-                console.error(`Couldn't write to log file: ${path}`);
-            }
-            fs.close(fd, () => {
-                buffer = null;
-                if(callback)
-                    callback();
-            });
-        });
+    fs.write(fd, buffer, 0, buffer.length, null, function(error) {
+      if (error) {
+        console.error(`Couldn't write to log file: ${path}`);
+      }
+      fs.close(fd, () => {
+        buffer = null;
+        if(callback)
+          callback();
+      });
     });
+  });
 }
 
-module.exports = log;
+exports.logSync = (message, level) => {
+  if(process.platform === 'darwin') {
+    path = __dirname + '/server.log';
+    time = Date().split(' ').slice(0,-2).join(' ');
+    line_ending = '\n';
+  }
+  else if(process.platform === 'win32') {
+    path = __dirname + '\\server.log';
+    time = Date().split(' ').slice(0,-4).join(' ');
+    line_ending = '\r\n';
+  }
+  var l = 'ERROR';
+  switch(level) {
+    case 'error':
+      l = 'ERROR';
+      break;
+    case 'warning':
+      l = 'WARNING';
+      break;
+    case 'debug':
+      l = 'DEBUG';
+      break;
+    case 'info':
+      l = 'INFO';
+      break;
+  }
+  var log_message = `${time} - ${l}: ${message}.${line_ending}`;
+  var buffer = new Buffer(log_message);
+  try {
+    var fd = fs.openSync(path, 'a');
+  } catch(e) {
+    console.error(`Couldn't open log file: ${path}`);
+  }
+  try {
+    fs.writeSync(fd, buffer, 0, buffer.length, null);
+  } catch(e) {
+    console.error(`Couldn't write to log file: ${path}`);
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 chalk = require('chalk');
 ws = require('ws');
 net = require('net');
-log = require('./logger.js');
+logger = require('./logger.js');
 
 const info = 'info';
 const debug = 'debug';
 const warning = 'warning';
 
-log('Server started', info);
+logger.log('Server started', info);
 
 error = chalk.bold.red;
-packet = chalk.magenta;
+packet = chalk.cyan;
 connect = chalk.green;
 disconnect = chalk.red;
 w = chalk.yellow;
-tcp = chalk.cyan;
+tcp = chalk.magenta;
 
 websocketClient = undefined;
 wsPort = 9002;
@@ -23,67 +23,66 @@ tcpClient = undefined;
 tcpPort = 9001;
 
 const wss = new ws.Server({
-    perMessageDeflate: false,
-    port: wsPort
+  perMessageDeflate: false,
+  port: wsPort
 });
 
 wss.on('connection', (ws) => {
-    ws.on('message', (m) => {
-        console.log(`Received on ${w('Websocket')}: ${packet(m)}`);
-        tcpClient.write(m);
-        console.log(`Sent on ${tcp('TCP')}: ${packet(m)}`);
-    });
-    ws.on('close', () => {
-        console.log(`${w('Websocket client')} has ${disconnect('disconnected')}.`);
-        log('Websocket client has disconnected', info);
-        websocketClient = undefined;
-    })
-    ws.on('error', (e) => {
-        console.log(`An ${error('error')} has occured: ${error(e.message)}`);
-        log(e.message);
-        websocketClient = undefined;
-    });
-    console.log(`${w('Websocket client')} has ${connect('connected')}.`);
-    log('Websocket client has connected', info);
-    websocketClient = ws;
+  ws.on('message', (m) => {
+    console.log(`Received on ${w('Websocket')}: ${packet(m)}`);
+    if(tcpClient) {
+      tcpClient.write(m);
+      console.log(`Sent on ${tcp('TCP')}: ${packet(m)}`);
+    }
+  });
+  ws.on('close', () => {
+    console.log(`${w('Websocket client')} has ${disconnect('disconnected')}.`);
+    logger.log('Websocket client has disconnected', info);
+    websocketClient = undefined;
+  })
+  ws.on('error', (e) => {
+    console.log(`An ${error('error')} has occured: ${error(e.message)}`);
+    logger.log(e.message);
+    websocketClient = undefined;
+  });
+  console.log(`${w('Websocket client')} has ${connect('connected')}.`);
+  logger.log('Websocket client has connected', info);
+  websocketClient = ws;
 })
     
 const nss = net.createServer((c) => {
-    tcpClient = c;
-    console.log(`${tcp('TCP client')} has ${connect('connected')}.`);
-    log('TCP client has connected', info);
-    c.on('end', () => {
-        console.log(`${tcp('TCP client')} has ${disconnect('disconnected')}.`);
-        log('TCP client has disconnected', info);
-    });
-    c.on('data', (m) => {
-            console.log(`Received on ${tcp('TCP')}: ${packet(m)}`);
-        if(websocketClient) {
-            if (websocketClient.readyState === 1) {
-                websocketClient.send(m);
-                console.log(`Sent on ${w('Websocket')}: ${packet(m)}`);
-            }
-        } else {
-            console.log(`Not connected to ${w('Websocket')}.`);
-        }
-    });
-    c.on('error', (e) => {
-        console.log(`An ${error('error')} has occured: ${error(e.message)}`);
-        log(e.message);
-    });
+  tcpClient = c;
+  console.log(`${tcp('TCP client')} has ${connect('connected')}.`);
+  logger.log('TCP client has connected', info);
+  c.on('end', () => {
+    console.log(`${tcp('TCP client')} has ${disconnect('disconnected')}.`);
+    logger.log('TCP client has disconnected', info);
+    tcpClient = undefined;
+  });
+  c.on('data', (m) => {
+      console.log(`Received on ${tcp('TCP')}: ${packet(m)}`);
+    if(websocketClient) {
+      if (websocketClient.readyState === 1) {
+        websocketClient.send(m);
+        console.log(`Sent on ${w('Websocket')}: ${packet(m)}`);
+      }
+    }
+  });
+  c.on('error', (e) => {
+    console.log(`An ${error('error')} has occured: ${error(e.message)}`);
+    logger.log(e.message);
+    tcpClient = undefined;
+  });
 });
 
 nss.listen(tcpPort, () => {
-    console.log(`${tcp('TCP server')} listening on port ${tcp(tcpPort)}.`);
+  console.log(`${tcp('TCP server')} listening on port ${tcp(tcpPort)}.`);
 });
 
-process.on('SIGINT', () => {
-    process.stdout.write("\033[2K\033[200D");
-    if(process.platform === 'win32')
-        process.stdout.write('Shutting down server.');
-    else if(process.platform === 'darwin')
-        console.log('Shutting down server.');
-    log('Server shut down', info, () => {
-        process.exit(); 
-    });
+process.on('SIGINT', () => { process.exit() });
+
+process.on('exit', () => {
+  process.stdout.write("\033[2K\033[200D");
+  logger.logSync('Server shut down', info);
+  console.log('Shutting down server.');
 });
